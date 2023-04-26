6ede2a7d806a1efe3762d59f5381821d28519dac
yashshingade28 <yashshingade28@protonmail.com>
2023-04-27 02:12:44 +0530

    setup.sh

--- zsh/conf/env.zsh
+++ zsh/conf/env.zsh
@@ -1,8 +1,32 @@
 alias cp="cp -i"                                                # Confirm before overwriting something
-alias df='df -h'                                                # Human-readable sizes
 alias free='free -m'                                            # Show sizes in MB
 
+export TERM="xterm-256color"
+
+# been there
 if command -v trash &> /dev/null
 then
   alias rm=trash
+fi
+
+# better df
+if command -v duf &> /dev/null
+then
+  alias df=duf
+fi
+
+# colored ls
+if command -v exa &> /dev/null
+then
+  alias ls=exa
+else
+  # File and Dir colors for ls and other outputs
+  export LS_OPTIONS='--color=auto'
+  eval "$(dircolors -b)"
+  alias ls='ls $LS_OPTIONS'
+fi
+
+if command -v bat &> /dev/null
+then
+  alias cat=bat
 fi
