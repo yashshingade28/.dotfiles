6cbc5832e3c149dff57cb98f9952672ef2fdbcdd
yashshingade28 <yashshingade28@protonmail.com>
2023-05-02 23:02:18 +0530

    added kitty setup

--- setup.sh
+++ setup.sh
@@ -2,15 +2,22 @@
 
 set -euo pipefail
 
+backup_if_exists () {
+  if [[ -r "$1" ]]; then
+    mv --backup=numbered "$1" "$1.bak"
+    return 0
+  fi
+}
+
 if [ $# -ne 1 ]; then
   echo "Error: This script requires exactly one command line argument." >&2
   exit 1
 fi
 
 setup_mode=$1
-DTF="$HOME/.dotfiles"
+DOTFILES="$HOME/.dotfiles"
 
-if [ ! -d "$DTF" ]; then
+if [ ! -d "$DOTFILES" ]; then
   echo ".dotfiles directory does not exist in the user's HOME('$HOME')." >&2
   exit 1
 fi
@@ -18,28 +25,39 @@ fi
 setup_zsh () {
   if ! command -v zsh &> /dev/null; then
     echo "zsh is not installed on this system." >&2
-    exit 1
+    return 1
   fi
 
-  if [ -e "$HOME/.zshrc" ]; then
-    mv --backup=numbered "$HOME/.zshrc" "$HOME/.zshrc.bak"
-  fi
+  backup_if_exists "$HOME/.zshrc"
 
-  ln -s "$DTF/zsh/.zshrc" "$HOME/.zshrc"
+  ln -s "$DOTFILES/zsh/.zshrc" "$HOME/.zshrc"
   zsh
   source "$HOME/.zshrc"
 }
 
 setup_fonts () {
-  if [ -d "$HOME/.local/share/fonts" ] || [ -L "$HOME/.local/share/fonts" ]; then
-    rm -r "$HOME/.local/share/fonts"
+  mkdir -p "$HOME/.local/share"
+  backup_if_exists "$HOME/.local/share/fonts"
+  ln -s "$DOTFILES/fonts" "$HOME/.local/share/fonts"
+}
+
+setup_kitty () {
+  if ! command -v kitty &> /dev/null; then
+    echo "kitty is not installed on this system." >&2
+    return 1
   fi
-  ln -s "$DTF/fonts" "$HOME/.local/share/fonts"
+
+  mkdir -p "$HOME/.config"
+  backup_if_exists "$HOME/.config/kitty"
+  ln -s "$DOTFILES/kitty" "$HOME/.config/kitty"
 }
 
 case "$setup_mode" in
   zsh)
     setup_zsh
+    ;;
+  kitty)
+    setup_kitty
     ;;
   fonts)
     setup_fonts
