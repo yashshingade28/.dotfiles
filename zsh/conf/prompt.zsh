32f49fdc66c477451d7f5849f43323766d1d3935
yashshingade28 <yashshingade28@protonmail.com>
2023-05-04 23:34:34 +0530

    improved prompt script

--- zsh/conf/prompt.zsh
+++ zsh/conf/prompt.zsh
@@ -2,19 +2,19 @@
   emulate -L zsh
 
   source_if_exists $DOTFILES/zsh/conf/powerlevel10k/powerlevel10k.zsh-theme
-  
+
   typeset USE_POWERLINE=true
 
   # Determine terminal capabilities.
   {
     if ! zmodload zsh/langinfo zsh/terminfo ||
-       [[ $langinfo[CODESET] != (utf|UTF)(-|)8 || $TERM == (dumb|linux) ]] ||
-       (( terminfo[colors] < 256 )); then
+      [[ $langinfo[CODESET] != (utf|UTF)(-|)8 || $TERM == (dumb|linux) ]] ||
+      (( terminfo[colors] < 256 )); then
       # Don't use the powerline config. It won't work on this terminal.
       USE_POWERLINE=false
       # Define alias `x` if our parent process is `login`.
-      typeset parent
-      if { parent=$(</proc/$PPID/comm) } && [[ ${parent:t} == login ]]; then
+      typeset parent=$(</proc/$PPID/comm:t)
+      if [[ $parent == login ]]; then
         alias x='startx ~/.xinitrc'
       fi
     fi
@@ -27,9 +27,9 @@
   else
     # Use 256 colors and UNICODE.
     if source_if_exists $DOTFILES/zsh/conf/.p10k.zsh; then
-	# Enabling the correct and wrong status at the right end
-    	typeset -g POWERLEVEL9K_STATUS_OK=true
-	typeset -g POWERLEVEL9K_STATUS_ERROR=true
+      # Enabling the correct and wrong status at the right end
+      typeset -g POWERLEVEL9K_STATUS_OK=true
+      typeset -g POWERLEVEL9K_STATUS_ERROR=true
     fi
     ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244'
   fi
